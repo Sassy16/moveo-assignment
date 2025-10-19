58dc4be53b3aa8869f8a8c27e2df5d3982e715d5
Sassy16 <giladbadin121@gmail.com>
2025-10-20 02:33:07 +0300

    fixed start server in index.js

--- server/index.js
+++ server/index.js
@@ -58,4 +58,4 @@ io.on('connection', (socket) => {
 
 // Start server
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+server.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
