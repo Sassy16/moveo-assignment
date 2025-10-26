65065e26f04bd5ff96bc8657e8b9c97580421f28
Sassy16 <giladbadin121@gmail.com>
2025-10-26 19:24:30 +0200

    Works well locally, fixed iPhone bugs at live page

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "https://moveo-assignment-production-6a25.up.railway.app";
+export const API_BASE_URL = "http://10.0.0.22:4000";
+export const API_BASE_URL2 = "https://moveo-assignment-production-6a25.up.railway.app";
--- client/src/pages/Live.js
+++ client/src/pages/Live.js
@@ -153,5 +153,6 @@ export default function Live() {
         )}
       </div>
     </div>
+
   );
 }
