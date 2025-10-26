ee9628ce63d198c81f683ef42413257f458316c9
Sassy16 <giladbadin121@gmail.com>
2025-10-26 19:39:04 +0200

    Prepare for redeploy - Update package.json files and config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,4 @@
-export const API_BASE_URL = "http://10.0.0.22:4000";
-export const API_BASE_URL2 = "https://moveo-assignment-production-6a25.up.railway.app";
+export const API_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? ""
+    : "http://localhost:4000";
