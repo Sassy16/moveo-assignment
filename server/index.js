daea7d9cbf614426b0a9c4eb365e9dbd2cd5cdae
Sassy16 <giladbadin121@gmail.com>
2025-10-26 20:09:17 +0200

    regex fix in index.js

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(clientPath));
 
   // Handle React routing, return all requests to index.html
-  app.get("/*", (req, res) => {
+  app.get("/.*/", (req, res) => {
     res.sendFile(path.join(clientPath, "index.html"));
   });
 }
